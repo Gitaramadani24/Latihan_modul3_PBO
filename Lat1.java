2627fa11bcb1779ed21c3d76e85850c6637252ed
Gitaramadani24 <114290893+Gitaramadani24@users.noreply.github.com>
2022-11-25 10:32:16 +0700

    Add files via upload

---
+++ Lat1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_modul3.pbo;
+
+/**
+ *
+ * @author user
+ */
+class Lat1 {
+ public static void main(String[] args) {
+        System.out.println("welcome to java programming Gita ramadani"); 
+    }
+}
